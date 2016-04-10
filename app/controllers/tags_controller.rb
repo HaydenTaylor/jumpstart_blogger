5ba7c6b9567329d7f23377a1e611f103cbb210d2
ArminusVarminus <garbagefilter@gmail.com>
2016-04-10 13:03:25 -0400

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,27 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params #needed? copying from CommentsController
+    params.require(:tag).permit()
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    @tag.destroy
+
+    #redirect_to tags_path
+  end
+
+
+
+
+end
